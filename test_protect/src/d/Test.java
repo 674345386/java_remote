ae6dacae7fb47ee3d704ce2d2b6f001fec236782
CP <674345386@qq.com>
2016-05-30 21:19:10 +0800

    add foreach

--- test_protect/src/d/Test.java
+++ test_protect/src/d/Test.java
@@ -19,7 +19,10 @@ public class Test {
 		System.out.println(b+5);  //会自动转成 int
 		System.out.printf("%c",b+5);
 		
-		
+		int[] c={5,4,10,3};
+		for (int i : c) {
+			System.out.println(i);
+		}
 		byte a1 =47;
 		byte b1 =35;
 //		byte c1 =a1+b1;    错误，a1+b1会先转成int 型
